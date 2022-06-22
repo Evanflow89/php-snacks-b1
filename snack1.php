155719c58110fc4f51579536b293f942b8ed4142
Evanflow89 <ivanbaragone@gmail.com>
2022-06-22 15:53:41 +0200

    first commit

---
+++ snack1.php
@@ -0,0 +1,28 @@
+<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60  -->
+
+<?php 
+
+$games = [
+    [
+        "homeTeam" => "Chicago Bulls",
+        "guestTeam" => "Milwaukee Bucks"
+        "homeScore" => 90,
+        "guestTeam" => 103,
+    ],
+    [
+        "homeTeam" => "Toronto Raptors",
+        "guestTeam" => "Orlando Magic"
+        "homeScore" => 99,
+        "guestTeam" => 77,
+    ],
+    [
+        "homeTeam" => "Detroit Pistons",
+        "guestTeam" => "Indiana Pacers"
+        "homeScore" => 109,
+        "guestTeam" => 88,
+    ],
+]
+
+?>
+
