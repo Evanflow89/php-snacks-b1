091b19659261292e913199aab75367a6ac69c0ba
Evanflow89 <ivanbaragone@gmail.com>
2022-06-22 16:55:54 +0200

    push

--- snack2.php
+++ snack2.php
@@ -1,12 +1,17 @@
 <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
-
+(
 
 
 <?php
 
 $userName = $_GET["name"];
 $userMail = $_GET["email"];
-$userPassword = $_GET["password"];
+$userAge = $_GET["age"];
 
+if (strlen($userName) >= 3 && (!filter_var($userMail, FILTER_VALIDATE_EMAIL) === false) && !is_nan($userAge)){
+    echo "Accesso Riuscito";
+} else {
+    echo "Accesso Negato";
+}
 
 ?>
